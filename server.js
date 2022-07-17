bc455e9acd7de9a4ba1e5db4e680185f3f22e3f4
gabypega1997 <gabypega1997@gmail.com>
2022-07-17 09:48:52 +0200

    add some logic

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const express = require('express');
 
 
+const port = "8080";
 const app = express();
 
-app.get("/", (req, res )=> {
-    res.send('hello world');
+app.use(express.json());
+
+app.get("/tshirt", (req, res )=> {
+    res.status(200).send({
+        tshirty: 'ts',
+        size: "large"
+    })
+})
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if(!logo){
+        res.status(418).send({message: 'We need a logo!'})
+    }
+
+    res.send({
+        tshirt: `ts with your ${logo} and ID of ${id}`
+    })
+
 })
 
 
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000')
+app.listen(port, () => {
+    console.log(`app is running on port http://localhost:${port}`)
 })
